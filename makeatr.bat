d85e3ab04fa1ea0d232939f86eb510a9593da551
MADRAFi <madrafi@gmail.com>
2023-04-17 01:44:50 +0200

    adding screenshots and atr creation

---
+++ makeatr.bat
@@ -0,0 +1,2 @@
+copy d:\Dropbox\Atari\DEV\PMAX_Update\pmax_upd.obx d:\Dropbox\Atari\DEV\PMAX_Update\disk\pm_upd.ar0
+d:\Atari\Soft\tools\dir2atr -md -b MyDos4534 d:\Dropbox\Atari\DEV\PMAX_Update\pmax.atr d:\Dropbox\Atari\DEV\PMAX_Update\Disk\
